660fc97acb704bddd21c4d2de663529b716095e6
Chathuka Dilakshana <chathukadilakshana33@gmail.com>
2025-05-20 22:47:05 +0530

    fix: Update static file serving path for production environment

--- server/server.js
+++ server/server.js
@@ -37,12 +37,9 @@ dbConnect()
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, "client", "build")));	
-
-  // The "catchall" handler: for any request that doesn't match one above, send back index.html.
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
   });
 }
 
