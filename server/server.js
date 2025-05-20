ad2d8ac2067de7f5e8c64e49728dd97672d2b66f
Chathuka Dilakshana <chathukadilakshana33@gmail.com>
2025-05-20 22:43:16 +0530

    fix: Correct path for serving static files in production environment

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ const app = express();
 
 if (process.env.NODE_ENV === "production") {
   // Serve static files from the React app
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));	
 
   // The "catchall" handler: for any request that doesn't match one above, send back index.html.
   app.get("*", (req, res) => {
