316a2c5a2ab020e5db797568a5876af80a7342ae
Chathuka Dilakshana <chathukadilakshana33@gmail.com>
2025-05-21 12:00:12 +0530

    feat: update API endpoints to use production URL for cart, order, products, reviews, and search functionalities

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -13,7 +13,7 @@ export const createNewOrder = createAsyncThunk(
   "/order/createNewOrder",
   async (orderData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/order/create",
+      "https://community-empowerment-hub-313ac18da07a.herokuapp.com/api/shop/order/create",
       orderData
     );
 
@@ -25,7 +25,7 @@ export const capturePayment = createAsyncThunk(
   "/order/capturePayment",
   async ({ paymentId, payerId, orderId }) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/order/capture",
+      "https://community-empowerment-hub-313ac18da07a.herokuapp.com/api/shop/order/capture",
       {
         paymentId,
         payerId,
@@ -41,7 +41,7 @@ export const getAllOrdersByUserId = createAsyncThunk(
   "/order/getAllOrdersByUserId",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/order/list/${userId}`
+      `https://community-empowerment-hub-313ac18da07a.herokuapp.com/api/shop/order/list/${userId}`
     );
 
     return response.data;
@@ -52,7 +52,7 @@ export const getOrderDetails = createAsyncThunk(
   "/order/getOrderDetails",
   async (id) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/order/details/${id}`
+      `https://community-empowerment-hub-313ac18da07a.herokuapp.com/api/shop/order/details/${id}`
     );
 
     return response.data;
--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/search/${keyword}`
+      `https://community-empowerment-hub-313ac18da07a.herokuapp.com/api/shop/search/${keyword}`
     );
 
     return response.data;
